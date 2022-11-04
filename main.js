1309f057aa17b8a48fdcb09ae391db221553fed7
Juliet Wong Min <julietwongmin@gmail.com>
2022-11-04 16:38:53 +1100

    Intro to Vue 3 lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    // short-hand data(){}
+    // data: function() {}
+    //
+    data(){
+        return{
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks.'
+        }
+    }
+})
