482536a666ee89d37b57ef36ad12561c7ffd3be2
Juliet Wong Min <julietwongmin@gmail.com>
2022-11-04 16:55:04 +1100

    Lesson 3

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const app = Vue.createApp ({
     data(){
         return{
             product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url:'https://vuejs.org/',
             description: 'A warm fuzzy pair of socks.'
         }
     }
