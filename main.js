318638bcac3df4c30d5632dda0169202fb81b976
Juliet Wong Min <julietwongmin@gmail.com>
2022-11-07 15:24:13 +1100

    Module 6

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-const app = Vue.createApp ({
-    // short-hand data(){}
-    // data: function() {}
-    //
-    data(){
-        return{
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url:'https://vuejs.org/',
-            description: 'A warm fuzzy pair of socks.',
+            image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool'],
-            sizes: ['S', 'M', 'L', 'XL'],
+            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                    {id: 2234, color: 'green'},
-                    {id: 2235, color:'blue'},
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1
+        },
+        removeToCart(){
+            if (this.cart >= 1) {
+            this.cart -=1
+            }
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
