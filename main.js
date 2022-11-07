bdec6d580d9b13e371836ed1eb80c7b5fa5af478
Juliet Wong Min <julietwongmin@gmail.com>
2022-11-07 15:00:22 +1100

    module 5

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ const app = Vue.createApp ({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             url:'https://vuejs.org/',
-            description: 'A warm fuzzy pair of socks.'
+            description: 'A warm fuzzy pair of socks.',
+            inStock: true,
+            details: ['50% cotton', '30% wool'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                    {id: 2234, color: 'green'},
+                    {id: 2235, color:'blue'},
+            ]
         }
     }
 })
